41550856b321d0d204838d9eae3c5a94e3e248bf
Henry Omar Garcia Ortiz <hheyomar16@gmail.com>
2019-12-12 16:34:39 -0300

    Update seed.groovy

--- resources/seed.groovy
+++ resources/seed.groovy
@@ -3,6 +3,4 @@ import groovy.json.JsonSlurper
 folder('POC') {
     description('Folder containing all jobs for folder-a')
 }
-
-def string = readFileFromWorkspace("${ntbfile}")
-def json = new JsonSlurper().parseText(string)
+def json = new JsonSlurper().parseText("${ntbfile")
