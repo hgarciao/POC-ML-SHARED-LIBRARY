fe6391572415f092748673206f674fa1c515d681
Henry Omar Garcia Ortiz <hheyomar16@gmail.com>
2019-12-12 17:49:33 -0300

    Update seed.groovy

--- resources/seed.groovy
+++ resources/seed.groovy
@@ -5,5 +5,36 @@ folder('POC') {
 }
 
 def jsn = new JsonSlurper().parseText("${ntbfile}")
-def imprt = jsn.cells.findAll{ it.tags.contains('imports')}
-out.println(imprt)
+def imports = []
+def parameters = []
+def preproc = []
+def preproc_test = []
+def trainning = []
+def trainning_test = []
+
+def imprt = jsn.cells.each{ 
+    if( it.metadata.tags.imports ) {
+        imports = imports << it
+    }else if(it.metadata.tags.parameters){
+        parameters = parameters << it
+    }else if(it.metadata.tags.preproc){
+        if(it.metadata.tags.test){
+            preproc_test = preproc_test << it
+        }else{
+            preproc = preproc << it
+        }
+    }else if(it.metadata.tags.trainning){
+        if(it.metadata.tags.test){
+            trainning_test = trainning_test << it
+        }else{
+            preproc = preproc << it
+        }
+    }
+}
+
+out.println(imports)
+out.println(parameters)
+out.println(preproc)
+out.println(preproc_test)
+out.println(trainning)
+out.println(trainning_test)
