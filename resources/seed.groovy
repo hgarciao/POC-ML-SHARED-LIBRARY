43daea853c363e58ccb0f949bb81c4bef733fb7c
Henry Omar Garcia Ortiz <hheyomar16@gmail.com>
2019-12-12 16:00:03 -0300

    Update seed.groovy

--- resources/seed.groovy
+++ resources/seed.groovy
@@ -1,13 +1,13 @@
 def project = 'quidryan/aws-sdk-test'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-out.println("MESSAGE : ${message}")
 branches.each {
     def branchName = it.name
     def jobName = "${project}-${branchName}".replaceAll('/','-')
     job(jobName) {
         steps {
-            out.println("MESSAGE : ${message}")
+            out.println("MESSAGE : ${branch}")
+            out.println("MESSAGE : ${nbtfile}")
         }
     }
 }
