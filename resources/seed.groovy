13cc31cde1658722a82d2dcd3a96f64e3a9d6c12
Henry Omar Garcia Ortiz <hheyomar16@gmail.com>
2019-12-12 12:34:09 -0300

    Update seed.groovy

--- resources/seed.groovy
+++ resources/seed.groovy
@@ -1,16 +1,17 @@
-
-folder('POC') {
-    description('Folder containing all jobs for folder-a')
-}
-
-job('POC/DSL-Tutorial-1-Test') {
-    scm {
-        git('git://github.com/quidryan/aws-sdk-test.git')
-    }
-    triggers {
-        scm('H/15 * * * *')
-    }
-    steps {
-        maven('-e clean test')
+prinltn "MESSAGE : ${message}"
+prinltn "CREDENTIALS : ${credentials}"
+def project = 'quidryan/aws-sdk-test'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            maven("test -Dproject.name=${project}/${branchName}")
+        }
     }
 }
