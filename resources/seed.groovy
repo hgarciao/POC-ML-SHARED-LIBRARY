33619fa8cc8f21b7978a405ebced434cecec0b4d
Henry Omar Garcia Ortiz <hheyomar16@gmail.com>
2019-12-12 17:26:43 -0300

    Update seed.groovy

--- resources/seed.groovy
+++ resources/seed.groovy
@@ -4,7 +4,9 @@ folder('POC') {
     description('Folder containing all jobs for folder-a')
 }
 
-def json = new JsonSlurper().parseText("${ntbfile}")
-out.println(json.getClass())
+def jsn = new JsonSlurper().parseText("${ntbfile}")
+out.println(jsn.get('cells'))
+out.println(jsn.get('cells').getClass())
+out.println(jsn.cells)
 /*def imprt = json.cells.findAll{ it.metadata.tags.contains('imports')}
 out.println(imprt)*/
