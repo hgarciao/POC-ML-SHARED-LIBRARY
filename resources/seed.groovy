cc3fe8bb09b6373725f7b53cbee9ff905f8ebe94
Henry Omar Garcia Ortiz <hheyomar16@gmail.com>
2019-12-13 11:40:23 -0300

    Update seed.groovy

--- resources/seed.groovy
+++ resources/seed.groovy
@@ -1,79 +1,12 @@
-/*import groovy.json.JsonSlurper
-
-folder('POC') {
-    description('Folder containing all jobs for folder-a')
-}
-
-def jsn = new JsonSlurper().parseText("${ntbfile}")
-def imports = []
-def parameters = []
-def preproc = []
-def preproc_test = []
-def trainning = []
-def trainning_test = []
-def results = []
-
-def imprt = jsn.cells.each{ 
-    if(it.metadata.tags){
-        if( it.metadata.tags.contains('imports')) {
-            imports = imports << it
-        }else if(it.metadata.tags.contains('parameters')){
-            parameters = parameters << it
-        }else if(it.metadata.tags.contains('preproc')){
-            if(it.metadata.tags.contains('test')){
-                preproc_test = preproc_test << it
-            }else{
-                preproc = preproc << it
-            }
-        }else if(it.metadata.tags.contains('trainning')){
-            if(it.metadata.tags.contains('test')){
-                trainning_test = trainning_test << it
-            }else{
-                trainning = trainning << it
-            }
-        }else if(it.metadata.tags.contains('results')){
-                results = results << it
-        }
-    }
-}
-
-pipelineJob('POC/job-name') {
-  definition {
-    cps {
-      script('''
-        pipeline {
-            agent any
-                stages {
-                    stage('Stage 1') {
-                        steps {
-                            echo "${branch}"
-                        }
-                    }
-                    stage('Stage 2') {
-                        steps {
-                            echo 'logic'
-                        }
-                    }
-                }
-            }
-        }
-      '''.stripIndent())
-      sandbox()     
-    }
-  }
-}
-*/
 folder("${project}") {
     description('Folder containing all jobs for folder-a')
 }
 
-pipelineJob("Pipeline") {
-    displayName('Pipeline')
-        definition {
-            cps { 
-                script(readFileFromWorkspace(pipeline.groovy))
-            }
-        }
+pipelineJob('job-name') {
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipeline.groovy'))
+      sandbox()     
     }
-
-
+  }
+}
